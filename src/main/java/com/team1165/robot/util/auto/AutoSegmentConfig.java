fb1b36fe5c8447f8dd2616b265ec31c0b82a8aee
Ashton A. <uhdbits@duck.com>
2025-03-05 11:58:34 -0700

    Fix up AutoSegmentConfig, work on ChoreoAuto

--- src/main/java/com/team1165/robot/util/auto/AutoSegmentConfig.java
+++ src/main/java/com/team1165/robot/util/auto/AutoSegmentConfig.java
@@ -9,13 +9,12 @@ package com.team1165.robot.util.auto;
 
 import com.team1165.robot.FieldConstants.*;
 import edu.wpi.first.units.measure.Time;
-import edu.wpi.first.wpilibj2.command.Command;
 
 /** An auto segment represents a segment of an auto, mainly consisting of a reef scoring location/level to score on and a coral station to go to after scoring, if wanted. Also can include delays between actions. */
-public class AutoSegment {
-  private final Reef.Location reefLocation;
-  private final Reef.Level reefLevel;
-  private final Time delayAfterScoring;
-  private final CoralStationLocation coralStation;
-  private final Time delayAfterCoralStation;
-}
+public record AutoSegmentConfig(
+    Reef.Location reefLocation,
+    Reef.Level reefLevel,
+    Time delayAfterScoring,
+    CoralStationLocation coralStation,
+    Time delayAfterIntake
+){};
--- src/main/java/com/team1165/robot/util/auto/ChoreoAuto.java
+++ src/main/java/com/team1165/robot/util/auto/ChoreoAuto.java
@@ -7,6 +7,38 @@
 
 package com.team1165.robot.util.auto;
 
+import static edu.wpi.first.units.Units.Seconds;
+
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.units.measure.Time;
+
 public class ChoreoAuto {
-  public enum
+  private final Time delayBeforeStart;
+  private final boolean pushPartner;
+  private final AutoSegmentConfig[] segments;
+
+  public ChoreoAuto(Time delayBeforeStart, boolean pushPartner, AutoSegmentConfig... segments) {
+    this.delayBeforeStart = delayBeforeStart;
+    this.pushPartner = pushPartner;
+    this.segments = segments;
+  }
+
+  public ChoreoAuto(Time delayBeforeStart, AutoSegmentConfig... segments) {
+    this(delayBeforeStart, false, segments);
+  }
+
+  public ChoreoAuto(boolean pushPartner, AutoSegmentConfig... segments) {
+    this(Seconds.zero(), pushPartner, segments);
+  }
+
+  public ChoreoAuto(AutoSegmentConfig... segments) {
+    this(Seconds.zero(), false, segments);
+  }
+
+  public AutoRoutine buildAuto(AutoRoutine routine, boolean resetOdometry) {
+    for (AutoSegmentConfig segment : segments) {
+      AutoTrajectory movementToScore = routine.trajectory()
+    }
+  }
 }
