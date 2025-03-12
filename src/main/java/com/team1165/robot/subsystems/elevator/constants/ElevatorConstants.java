083e805f61552020cf2770d3017cc4ff5c46b3aa
Ashton A. <uhdbits@duck.com>
2025-03-12 05:52:08 -0700

    Add flywheel subsystem, minor elevator changes
    Added the flywheel subsystem alongside some minor changes to the elevator subsystem.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/subsystems/elevator/constants/ElevatorConstants.java
+++ src/main/java/com/team1165/robot/subsystems/elevator/constants/ElevatorConstants.java
@@ -46,47 +46,4 @@ public class ElevatorConstants {
   public static final class RightMotorConstants {
     public static final int canID = 15;
   }
-
-  public static TalonFXConfiguration ELEVATOR_CONFIG = new TalonFXConfiguration();
-
-  static {
-    ELEVATOR_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    ELEVATOR_CONFIG.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-
-    ELEVATOR_CONFIG.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-    ELEVATOR_CONFIG.SoftwareLimitSwitch.ForwardSoftLimitThreshold = Inches.of(66).in(Inches);
-    ELEVATOR_CONFIG.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
-    ELEVATOR_CONFIG.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Inches.of(0).in(Inches);
-
-    ELEVATOR_CONFIG.Slot0.GravityType = GravityTypeValue.Elevator_Static;
-    ELEVATOR_CONFIG.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign;
-    // Elevator motors will provide feedback in INCHES the carriage has moved
-    ELEVATOR_CONFIG.Feedback.SensorToMechanismRatio =
-        (2 * Math.PI * (2 * sprocketRadiusInches))
-            / gearRatio; // TODO: Check and make sure this math is right!!!!
-    ELEVATOR_CONFIG.Slot0.kG = 0.3;
-    ELEVATOR_CONFIG.Slot0.kS = 0.4;
-    // ELEVATOR_CONFIG.Slot0.kP = 1;
-    ELEVATOR_CONFIG.Slot0.kP = 0.3;
-
-    ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimit = 80;
-    ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
-    ELEVATOR_CONFIG.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-    ELEVATOR_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-  }
-
-  //  public static final ElevatorConfig elevatorConfig =
-  //      switch (Constants.getRobot()) {
-  //        case COMPBOT -> new ElevatorConfig(4, 0, (1.0 / 2.0), 9000.0);
-  //        case DEVBOT -> new ElevatorConfig(5, 4, (1.0 / 2.0), 6000.0);
-  //        case SIMBOT -> new ElevatorConfig(0, 0, (1.0 / 2.0), 9000.0);
-  //      };
-
-  //  public static final Gains gains =
-  //      switch (Constants.getRobot()) {
-  //        case COMPBOT -> new Gains(0.18, 0, 0.0006, 0.38367, 0.00108, 0);
-  //        case DEVBOT -> new Gains(0.0003, 0.0, 0.0, 0.33329, 0.00083, 0.0);
-  //        case SIMBOT -> new Gains(0.05, 0.0, 0.0, 0.01, 0.00103, 0.0);
-  //      };
-
 }
--- src/main/java/com/team1165/robot/subsystems/elevator/io/ElevatorIOTalonFX.java
+++ src/main/java/com/team1165/robot/subsystems/elevator/io/ElevatorIOTalonFX.java
@@ -7,14 +7,20 @@
 
 package com.team1165.robot.subsystems.elevator.io;
 
+import static com.team1165.robot.subsystems.elevator.constants.ElevatorConstants.gearRatio;
+import static com.team1165.robot.subsystems.elevator.constants.ElevatorConstants.sprocketRadiusInches;
+
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 import com.team1165.robot.subsystems.elevator.constants.ElevatorConstants;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -50,13 +56,28 @@ public class ElevatorIOTalonFX implements ElevatorIO {
       new MotionMagicVoltage(0).withUpdateFreqHz(0.0);
 
   public ElevatorIOTalonFX() {
+    var ELEVATOR_CONFIG = new TalonFXConfiguration();
+
+    ELEVATOR_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    ELEVATOR_CONFIG.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+    ELEVATOR_CONFIG.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+    ELEVATOR_CONFIG.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign;
+    // Elevator motors will provide feedback in INCHES the carriage has moved
+    ELEVATOR_CONFIG.Feedback.SensorToMechanismRatio =
+        (2 * Math.PI * (2 * sprocketRadiusInches))
+            / gearRatio; // TODO: Check and make sure this math is right!!!!
+
+    ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimit = 80;
+    ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
+
     leftTalon = new TalonFX(ElevatorConstants.LeftMotorConstants.canID, ElevatorConstants.canBus);
     rightTalon = new TalonFX(ElevatorConstants.RightMotorConstants.canID, ElevatorConstants.canBus);
     rightTalon.setControl(new Follower(leftTalon.getDeviceID(), true));
 
     // Apply configs
-    rightTalon.getConfigurator().apply(ElevatorConstants.ELEVATOR_CONFIG, 1);
-    leftTalon.getConfigurator().apply(ElevatorConstants.ELEVATOR_CONFIG, 1);
+    rightTalon.getConfigurator().apply(ELEVATOR_CONFIG, 1);
+    leftTalon.getConfigurator().apply(ELEVATOR_CONFIG, 1);
     leftTalon
         .getConfigurator()
         .apply(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive));
