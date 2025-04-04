059d244f08dba1687697a2d932e99bf638792a78
Ashton A. <uhdbits@duck.com>
2025-04-04 11:43:36 -0700

    Start adding TalonMotorData, finish up RollerIOSparkMax
    Start adding TalonMotorData to log motor data from a Talon FX, and finish RollerIOSparkMax.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/util/logging/MotorData.java
+++ src/main/java/com/team1165/robot/util/logging/MotorData.java
@@ -7,44 +7,75 @@
 
 package com.team1165.robot.util.logging;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.Faults;
 import com.team1165.robot.util.vendor.rev.SparkUtil;
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import java.util.ArrayList;
+import java.util.List;
 import org.littletonrobotics.junction.LogTable;
 
 /**
- * Class that provides easy logging of all the values needed from a motor (controller) to log through
- * AdvantageKit IO classes.
+ * Class that provides easy logging of all the values needed from a motor (controller) to log
+ * through AdvantageKit IO classes.
  */
 public class MotorData {
   /** The applied voltage to the motor controller. */
   public double appliedVolts = 0.0;
+
   /** Whether the motor controller is connected. Might be unreliable with a SPARK. */
   public boolean connected = false;
+
   /** Whether a fault is currently active on the motor controller. */
   public boolean faultActive = false;
+
   /** String of the currently active faults on the motor controller. */
   public String faults = "";
+
   /** The temperature of the motor. */
   public double motorTemperatureCelsius = 0.0;
-  /** The output current of the motor (controller). This is the general output current on a SPARK and the torque current on a Talon. */
+
+  /**
+   * The output current of the motor (controller). This is the general output current on a SPARK and
+   * the torque current on a Talon.
+   */
   public double outputCurrentAmps = 0.0;
-  /** The position reported by the motor controller. Typically motor rotations, but could be different depending on the specific conversion factors on the motor controller. */
+
+  /**
+   * The position reported by the motor controller. Typically motor rotations, but could be
+   * different depending on the specific conversion factors on the motor controller.
+   */
   public double position = 0.0;
+
   /** The temperature of the motor controller. Not applicable on a SPARK. */
   public double processorTemperatureCelsius = 0.0;
+
   /** The supply current provided to the motor controller. */
   public double supplyCurrentAmps = 0.0;
-  /** The velocity reported by the motor controller. Typically reported in motor rotations per minute, but could be different depending on the specific conversion factors on the motor controller. */
+
+  /**
+   * The velocity reported by the motor controller. Typically reported in motor rotations per
+   * minute, but could be different depending on the specific conversion factors on the motor
+   * controller.
+   */
   public double velocity = 0.0;
 
   /**
-   * Method to log this {@link MotorData} to a {@link LogTable} from an AdvantageKit IO class. Call this inside your IO's {@code toLog()}. Make sure the {@code key} matches the one you call {@code fromLog()} with!
+   * Method to log this {@link MotorData} to a {@link LogTable} from an AdvantageKit IO class. Call
+   * this inside your IO's {@code toLog()}. Make sure the {@code key} matches the one you call
+   * {@code fromLog()} with!
    *
    * @param table The table to log to.
-   * @param key The key value to log everything under ("Primary" will log everything under the "Primary/*" key).
+   * @param key The key value to log everything under ("Primary" will log everything under the
+   *     "Primary/*" key).
    */
   public void toLog(LogTable table, String key) {
     table.put(key + "/AppliedVolts", appliedVolts);
@@ -60,10 +91,13 @@ public class MotorData {
   }
 
   /**
-   * Method to grab the values from a log of this {@link MotorData} from a {@link LogTable} of an AdvantageKit IO class. Call this inside your IO's {@code fromLog()}. Make sure the {@code key} matches the one you call {@code toLog()} with!
+   * Method to grab the values from a log of this {@link MotorData} from a {@link LogTable} of an
+   * AdvantageKit IO class. Call this inside your IO's {@code fromLog()}. Make sure the {@code key}
+   * matches the one you call {@code toLog()} with!
    *
    * @param table The table to grab logs from.
-   * @param key The key value to grab the logs from ("Primary" will grab everything under the "Primary/*" key).
+   * @param key The key value to grab the logs from ("Primary" will grab everything under the
+   *     "Primary/*" key).
    */
   public void fromLog(LogTable table, String key) {
     appliedVolts = table.get(key + "/AppliedVolts", appliedVolts);
@@ -73,7 +107,8 @@ public class MotorData {
     motorTemperatureCelsius = table.get(key + "/MotorTemperatureCelsius", motorTemperatureCelsius);
     outputCurrentAmps = table.get(key + "/OutputCurrentAmps", outputCurrentAmps);
     position = table.get(key + "/Position", position);
-    processorTemperatureCelsius = table.get(key + "/ProcessorTemperatureCelsius", processorTemperatureCelsius);
+    processorTemperatureCelsius =
+        table.get(key + "/ProcessorTemperatureCelsius", processorTemperatureCelsius);
     supplyCurrentAmps = table.get(key + "/SupplyCurrentAmps", supplyCurrentAmps);
     velocity = table.get(key + "/Velocity", velocity);
   }
@@ -84,10 +119,14 @@ public class MotorData {
     private final SparkBase spark;
     private final RelativeEncoder encoder;
 
-    /** Debouncer in order to ignore any skipped CAN frames instead of reporting that the controller is disconnected. */
+    /**
+     * Debouncer in order to ignore any skipped CAN frames instead of reporting that the controller
+     * is disconnected.
+     */
     private final Debouncer connectedDebouncer = new Debouncer(0.25);
 
-    /** Constructs a {@link SparkMotorData} using the specified constants.
+    /**
+     * Constructs a {@link SparkMotorData} using the specified constants.
      *
      * @param spark The {@link SparkBase} to log data from.
      */
@@ -96,9 +135,12 @@ public class MotorData {
       this.encoder = spark.getEncoder();
     }
 
-    /** Updates the motor data using the values from the REV SPARK motor controller linked with this instance. */
+    /**
+     * Updates the motor data using the values from the REV SPARK motor controller linked with this
+     * instance.
+     */
     public void update() {
-      // Reset the sticky fault status from SparkUtil
+      // Make sure the sticky fault status is reset from SparkUtil
       SparkUtil.sparkStickyFault = false;
       // Get applied output since it's used multiple times later on
       double appliedOutput = spark.getAppliedOutput();
@@ -120,8 +162,10 @@ public class MotorData {
       } else {
         faults = "";
       }
-      motorTemperatureCelsius = SparkUtil.ifOkOrDefault(spark, spark::getMotorTemperature, motorTemperatureCelsius);
-      outputCurrentAmps = SparkUtil.ifOkOrDefault(spark, spark::getOutputCurrent, outputCurrentAmps);
+      motorTemperatureCelsius =
+          SparkUtil.ifOkOrDefault(spark, spark::getMotorTemperature, motorTemperatureCelsius);
+      outputCurrentAmps =
+          SparkUtil.ifOkOrDefault(spark, spark::getOutputCurrent, outputCurrentAmps);
       position = SparkUtil.ifOkOrDefault(spark, encoder::getPosition, position);
       processorTemperatureCelsius = 0.0; // Not compatible with SPARKs
       supplyCurrentAmps = outputCurrentAmps * appliedOutput; // Approximate, not exact
@@ -129,6 +173,39 @@ public class MotorData {
 
       // After updating everything, check if SparkUtil reports any connection issues/sticky fault
       connected = connectedDebouncer.calculate(!SparkUtil.sparkStickyFault);
+      SparkUtil.sparkStickyFault = false; // Reset SparkUtil sticky fault status
+    }
+  }
+
+  public static class TalonMotorData extends MotorData {
+    private final TalonFX talon;
+
+    private final StatusSignal<Voltage> appliedVoltsSignal;
+    private final StatusSignal<Integer> faultFieldSignal;
+    private final List<StatusSignal<Boolean>> faultsSignals =
+        new ArrayList<StatusSignal<Boolean>>(99);
+    private final StatusSignal<Temperature> motorTemperatureSignal;
+    private final StatusSignal<Current> outputCurrentSignal;
+    private final StatusSignal<Angle> positionSignal;
+    private final StatusSignal<Temperature> processorTemperatureSignal;
+    private final StatusSignal<Current> supplyCurrentSignal;
+    private final StatusSignal<AngularVelocity> velocitySignal;
+
+    public TalonMotorData(TalonFX talon) {
+      this.talon = talon;
+
+      appliedVoltsSignal = talon.getMotorVoltage();
+      faultFieldSignal = talon.getFaultField();
+      faultsSignals.add(talon.getFault_BootDuringEnable());
+      faultsSignals.add(talon.getFault_DeviceTemp());
+      faultsSignals.add(talon.getFault_Hardware());
+      faultsSignals.add(talon.getFault_ProcTemp());
+      motorTemperatureSignal = talon.getDeviceTemp();
+      outputCurrentSignal = talon.getTorqueCurrent();
+      positionSignal = talon.getPosition();
+      processorTemperatureSignal = talon.getProcessorTemp();
+      supplyCurrentSignal = talon.getSupplyCurrent();
+      velocitySignal = talon.getVelocity();
     }
   }
 }
