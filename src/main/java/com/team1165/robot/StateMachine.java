7917a238809c9fd96bdd3d62598659ea793e9144
Ashton A. <uhdbits@duck.com>
2025-05-07 10:43:05 -0700

    Continue work on StateMachine
    Start adding goals to the state machine, other cleanup and organization.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/StateMachine.java
+++ src/main/java/com/team1165/robot/StateMachine.java
@@ -10,6 +10,8 @@
 package com.team1165.robot;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -41,7 +43,11 @@ public abstract class StateMachine<S extends Enum<S>> extends SubsystemBase {
     currentState = initialState;
   }
 
-  /** Periodic method called by the {@link edu.wpi.first.wpilibj2.command.CommandScheduler} each loop. This will update the inputs of the subsystem and switch it to the next state, if it has a next state and if it is ready. */
+  /**
+   * Periodic method called by the {@link edu.wpi.first.wpilibj2.command.CommandScheduler} each
+   * loop. This will update the inputs of the subsystem and switch it to the next state, if it has a
+   * next state and if it is ready.
+   */
   @Override
   public void periodic() {
     // Update the inputs of this subsystem
@@ -52,12 +58,35 @@ public abstract class StateMachine<S extends Enum<S>> extends SubsystemBase {
   }
 
   /**
+   * Creates a command that finishes once this subsystem is in the given state.
+   *
+   * @param goalState The state to wait for.
+   * @return A command that finishes once the current state is equal to the goal state.
+   */
+  public Command waitForState(S goalState) {
+    return Commands.waitUntil(() -> this.currentState == goalState);
+  }
+
+  public Command waitUntilGoalReached() {
+    return Commands.waitUntil(this::atGoal);
+  }
+
+  /**
    * Return the current state of this subsystem.
    *
    * @return The current state of this subsystem.
    */
   public S getCurrentState() {
     return currentState;
+  }
+
+  /** Returns if the subsystem has fully reached it's current state, if it's goal has been reached. This can be used to check to see if a subsystem has reached the current state's position, speed, etc.
+   *
+   * <p>By default, this returns true, and will only return other values if overridden.
+   * @return If the subsystem is at the state's goal.
+   */
+  protected boolean atGoal() {
+    return true;
   }
 
   protected S getNextState() {
@@ -72,23 +101,30 @@ public abstract class StateMachine<S extends Enum<S>> extends SubsystemBase {
   }
 
   /**
-   * Start a transition between two states. This method will log the current timestamp at the time
-   * of the transition, log the new state of the subsystem, and then call <code>afterTransition()
-   * </code> to actually perform the full transition and set the speed, position, etc., of the
-   * subsystem.
-   */
-  private void startTransition() {
-    lastTransitionTimestamp = Timer.getTimestamp();
-    Logger.recordOutput(this.getName() + "/CurrentState", currentState);
-  }
-
-  /**
    * Perform a transition between two states. This is where most of the logic of the state machine
    * actually exists, and where the user should change the speed, position, and more of the
    * subsystems. This will transition to the assigned current state of the subsystem.
    */
   protected void transition() {}
 
-  /** Update the inputs of this subsystem. This is typically done through an AdvantageKit-style IO class. */
+  /**
+   * Update the inputs of this subsystem. This is typically done through an AdvantageKit-style IO
+   * class.
+   */
   protected void updateInputs() {}
+
+  /**
+   * Start a transition to the new current state. This method will save the current timestamp at the
+   * time of the transition, log the new state of the subsystem, and then call {@link #transition()}
+   * to actually perform the full transition and set the speed, position, etc., of the subsystem.
+   */
+  private void startTransition() {
+    // Get the timestamp where the transition occurs
+    lastTransitionTimestamp = Timer.getTimestamp();
+    // Log the new state
+    Logger.recordOutput(this.getName() + "/CurrentState", currentState);
+
+    // Run the actual transition
+    transition();
+  }
 }
