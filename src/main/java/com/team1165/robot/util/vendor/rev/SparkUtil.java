90c6adf8b73cd31fbe1bda4baf2ca0d6213b52f4
Ashton A. <uhdbits@duck.com>
2025-05-13 14:02:19 -0700

    Work on adding more utilities to SparkUtil
    Work on adding a lot more utilities to SparkUtil, adding some comments as well.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/util/vendor/rev/SparkUtil.java
+++ src/main/java/com/team1165/robot/util/vendor/rev/SparkUtil.java
@@ -11,17 +11,75 @@ package com.team1165.robot.util.vendor.rev;
 
 import com.revrobotics.REVLibError;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
+/** Class containing various utilities to interface with SPARK motor controllers. */
 public class SparkUtil {
-  /** Stores whether any error was has been detected by other utility methods. */
-  public static boolean sparkStickyFault = false;
+  /** Stores whether any error was detected by any utility methods, since the last reset. */
+  private static boolean sparkStickyFault = false;
 
-  /** Processes a value from a Spark only if the value is valid. */
+  /**
+   * Creates and configures a SPARK ({@link SparkBase}) motor controller with the provided configuration.
+   *
+   * @param config The configuration of the SPARK motor controller.
+   * @param name The name of the SPARK motor controller (used for logging if anything goes wrong).
+   * @return The new {@link SparkBase} created.
+   */
+  public static SparkBase createNewSpark(SparkConfig config, String name) {
+    // Create the SPARK based on the model provided
+    var spark = config.model() == SparkModel.SparkFlex
+        ? new SparkFlex(config.canId(), config.motorType())
+        : new SparkMax(config.canId(), config.motorType());
+
+    // Configure the SPARK with the configuration given, and alert if it was never successful
+    new Alert(
+            "SPARK \"" + name + "\" (ID: " + config.canId() + ") configuration has failed. Unexpected behavior may occur.",
+            AlertType.kWarning)
+        .set(
+            !SparkUtil.tryUntilOk(
+                5,
+                () ->
+                    spark.configure(
+                        config.config(),
+                        ResetMode.kResetSafeParameters,
+                        PersistMode.kPersistParameters)));
+
+    return spark;
+  }
+
+  /**
+   * Get the sticky fault status. The sticky fault status represents if any methods called here since the last reset have returned an error.
+   *
+   * @return The sticky fault status.
+   */
+  public static boolean getStickyFault() {
+    return sparkStickyFault;
+  }
+
+  /**
+   * Reset the sticky fault status back to false. This should be called right before grabbing anything from a SPARK for the loop period.
+   */
+  public static void resetStickyFault() {
+    sparkStickyFault = false;
+  }
+
+  /**
+   * Processes a value from a SPARK only if the value is valid (does not error).
+   *
+   * @param spark The SPARK involved with this process.
+   * @param supplier The value to try to get from the SPARK.
+   * @param consumer The consumer that will process the value if valid.
+   */
   public static void ifOk(SparkBase spark, DoubleSupplier supplier, DoubleConsumer consumer) {
     double value = supplier.getAsDouble();
     if (spark.getLastError() == REVLibError.kOk) {
@@ -31,7 +89,13 @@ public class SparkUtil {
     }
   }
 
-  /** Processes a value from a Spark only if the value is valid. */
+  /**
+   * Processes values from a SPARK only if the values are valid (don't result in any errors).
+   *
+   * @param spark The SPARK involved with this process.
+   * @param suppliers The values to try to get from the SPARK.
+   * @param consumer The consumer that will process the values if valid.
+   */
   public static void ifOk(
       SparkBase spark, DoubleSupplier[] suppliers, Consumer<double[]> consumer) {
     double[] values = new double[suppliers.length];
@@ -69,13 +133,18 @@ public class SparkUtil {
     }
   }
 
-  /** Attempts to run the command until no error is produced. */
-  public static void tryUntilOk(int maxAttempts, Supplier<REVLibError> command) {
+  /**
+   * Attempts to run the method/supplier until no error is produced.
+   *
+   * @return If the method was ever successful.
+   */
+  public static boolean tryUntilOk(int maxAttempts, Supplier<REVLibError> command) {
     for (int i = 0; i < maxAttempts; i++) {
       var error = command.get();
       if (error == REVLibError.kOk) {
-        break;
+        return true;
       }
     }
+    return false;
   }
 }
