f4153319f11bb0fc0e64c48ddc3439684f19edd6
Ashton A. <uhdbits@duck.com>
2025-05-20 13:28:41 -0700

    Create GenericMotorData
    Create GenericMotorData, essentially just a simple class that allows values to be manually passed in when calling "update()", allowing the class to be applicable to essentially any simulation method (and also other motor controllers that don't already exist in MotorData).
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/util/logging/MotorData.java
+++ src/main/java/com/team1165/robot/util/logging/MotorData.java
@@ -122,6 +122,83 @@ public class MotorData {
   }
 
   /**
+   * Generic {@link MotorData} class used for any controllers that don't already exist in {@link
+   * MotorData}, and for simulation.
+   *
+   * <p>Values are manually passed in each time the {@link #update} method is called.
+   */
+  public static class GenericMotorData extends MotorData {
+    /**
+     * Updates the motor data using the values passed into this method. If any values do not exist
+     * (or for simulation, are not simulated), and are not required for a specific subsystem, pass
+     * in <code>0.0</code>.
+     *
+     * @param appliedVolts The value to set {@link #appliedVolts} to.
+     * @param connected The value to set {@link #connected} to.
+     * @param faultActive The value to set {@link #faultActive} to.
+     * @param faults The value to set {@link #faults} to.
+     * @param motorTemperatureCelsius The value to set {@link #motorTemperatureCelsius} to.
+     * @param outputCurrentAmps The value to set {@link #outputCurrentAmps} to.
+     * @param position The value to set {@link #position} to.
+     * @param processorTemperatureCelsius The value to set {@link #processorTemperatureCelsius} to.
+     * @param supplyCurrentAmps The value to set {@link #supplyCurrentAmps} to.
+     * @param velocity The value to set {@link #velocity} to.
+     */
+    public void update(
+        double appliedVolts,
+        boolean connected,
+        boolean faultActive,
+        String faults,
+        double motorTemperatureCelsius,
+        double outputCurrentAmps,
+        double position,
+        double processorTemperatureCelsius,
+        double supplyCurrentAmps,
+        double velocity) {
+      this.appliedVolts = appliedVolts;
+      this.connected = connected;
+      this.faultActive = faultActive;
+      this.faults = faults;
+      this.motorTemperatureCelsius = motorTemperatureCelsius;
+      this.outputCurrentAmps = outputCurrentAmps;
+      this.position = position;
+      this.processorTemperatureCelsius = processorTemperatureCelsius;
+      this.supplyCurrentAmps = supplyCurrentAmps;
+      this.velocity = velocity;
+    }
+
+    /**
+     * Updates the motor data using the values passed into this method. If any values do not exist
+     * (or for simulation, are not simulated), and are not required for a specific subsystem, pass
+     * in <code>0.0</code>.
+     *
+     * @param appliedVolts The value to set {@link #appliedVolts} to.
+     * @param outputCurrentAmps The value to set {@link #outputCurrentAmps} to.
+     * @param position The value to set {@link #position} to.
+     * @param supplyCurrentAmps The value to set {@link #supplyCurrentAmps} to.
+     * @param velocity The value to set {@link #velocity} to.
+     */
+    public void update(
+        double appliedVolts,
+        double outputCurrentAmps,
+        double position,
+        double supplyCurrentAmps,
+        double velocity) {
+      update(
+          appliedVolts,
+          true,
+          false,
+          "",
+          0.0,
+          outputCurrentAmps,
+          position,
+          0.0,
+          supplyCurrentAmps,
+          velocity);
+    }
+  }
+
+  /**
    * {@link MotorData} class that uses a REV SPARK (MAX/FLEX) motor controller with a relative
    * encoder to log data.
    */
@@ -323,7 +400,8 @@ public class MotorData {
       connectedAlert.set(
           !(connected =
               connectedDebouncer.calculate(
-                  BaseStatusSignal.isAllGood(motorTemperatureSignal, processorTemperatureSignal, supplyCurrentSignal))));
+                  BaseStatusSignal.isAllGood(
+                      motorTemperatureSignal, processorTemperatureSignal, supplyCurrentSignal))));
     }
   }
 }
