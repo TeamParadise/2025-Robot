ea8fffce8a8e4c2f60ca141005445cfc0c23665f
Ashton A. <uhdbits@duck.com>
2025-09-09 18:51:01 -0700

    Additional changes for SensorIO
    Forgot to stage all of the changes.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/subsystems/roller/flywheel/sensor/SensorIOCTRE.java
+++ src/main/java/com/team1165/robot/subsystems/roller/flywheel/sensor/SensorIOCTRE.java
@@ -1,4 +1,29 @@
+/*
+ * Copyright (c) 2025 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
 package com.team1165.robot.subsystems.roller.flywheel.sensor;
 
-public class SensorIOCANrange {
+import com.ctre.phoenix6.hardware.CANrange;
+import com.team1165.robot.subsystems.roller.flywheel.sensor.SensorIO.SensorIOInputs;
+import com.team1165.robot.util.vendor.ctre.PhoenixDeviceConfigs.CANrangeConfig;
+import com.team1165.robot.util.vendor.ctre.PhoenixUtil;
+
+/**
+ * A hardware interface/implementation layer for a CTRE CANrange sensor.
+ */
+public class SensorIOCTRE {
+  private final CANrange sensor;
+
+  public SensorIOCTRE(CANrangeConfig config) {
+    // Initialize CANrange
+    sensor = PhoenixUtil.createNewCANrange(config);
+  }
+
+  public void updateInputs(SensorIOInputs inputs) {
+    inputs.distance = sensor.getDistance().getValueAsDouble();
+  }
 }
