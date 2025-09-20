d89f312820c1e50c4994801486bdda5f7bcb0652
Ashton A. <uhdbits@duck.com>
2025-09-19 19:26:12 -0700

    Update at 'Fri Sep 19 19:26:12 MST 2025'

--- src/main/java/com/team1165/robot/subsystems/roller/flywheel/FlywheelState.java
+++ src/main/java/com/team1165/robot/subsystems/roller/flywheel/FlywheelState.java
@@ -15,7 +15,7 @@ public enum FlywheelState implements State {
   /** Idle, not moving. */
   IDLE(0.0),
   /** Default intake state to get coral into the scoring mechanism. */
-  INTAKE(1.32),
+  INTAKE(1.30),
   /** Slow scoring speed. */
   SLOW_SCORE(1.4),
   /** Fast scoring speed. */
