6df00652d44b06d4321e445d2c36cae644738cc3
Ashton A. <uhdbits@duck.com>
2025-10-03 17:56:14 -0700

    Update at 'Fri Oct 03 17:56:14 MST 2025'

--- src/main/java/com/team1165/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/com/team1165/robot/subsystems/elevator/ElevatorConstants.java
@@ -31,7 +31,7 @@ public class ElevatorConstants {
     // Base configuration creation
     public static final TalonFXConfiguration baseMotorConfig = new TalonFXConfiguration();
 
-    public static final double primaryZeroPosition = 0.41;
+    public static final double primaryZeroPosition = -0.23;
     public static final double secondaryZeroPosition = 0.12982191818 - 0.048;
 
     static {
